3a7badc69daa3c3d8e7b3222f0fe463a8be1b8ee
Jesus bossa <x@jesusbossa.com>
2020-04-28 16:47:19 -0500

    [ADD] Categories

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { ListOfCategories } from './components/ListOfCategories/ListOfCategories';
+import { GlobalStyles } from './globalStyles';
+
+export const App = () => (
+  <>
+    <GlobalStyles />
+    <ListOfCategories />
+  </>
+);
---
+++ src/components/ListOfCategories/ListOfCategories.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { List, Item } from './styles';
+import { Category } from '../Category/category';
+import { categories } from '../../../api/db.json';
+
+export const ListOfCategories = () => {
+  return (
+    <List>
+      {categories.map((category) => (
+        <Item key={category.id}>
+          <Category {...category} />
+        </Item>
+      ))}
+    </List>
+  );
+};
