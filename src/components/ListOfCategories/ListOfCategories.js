2ed41ea4142b3594a9e624bd43a551fa3e2bcbac
Jesus bossa <x@jesusbossa.com>
2020-04-29 14:51:38 -0500

    [ADD] Category loading

--- src/components/ListOfCategories/ListOfCategories.js
+++ src/components/ListOfCategories/ListOfCategories.js
@@ -2,15 +2,26 @@ import React, { useState, useEffect } from 'react';
 import { List, Item } from './styles';
 import { Category } from '../Category/category';
 
-export const ListOfCategories = () => {
+function useCategoriesData() {
   const [categories, setCategories] = useState([]);
-  const [showFixed, setFixed] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
+    setLoading(true);
     fetch('https://petgram-server-makinox.makinoxx.now.sh/categories')
       .then((res) => res.json())
-      .then((response) => setCategories(response));
+      .then((response) => {
+        setCategories(response);
+        setLoading(false);
+      });
   }, []);
+
+  return { categories, loading };
+}
+
+export const ListOfCategories = () => {
+  const { categories, loading } = useCategoriesData();
+  const [showFixed, setFixed] = useState(false);
 
   useEffect(() => {
     const onScroll = (e) => {
@@ -24,12 +35,18 @@ export const ListOfCategories = () => {
   }, [showFixed]);
 
   const renderList = (fixed) => (
-    <List className={fixed ? 'fixed' : ''}>
-      {categories.map((category) => (
-        <Item key={category.id}>
-          <Category {...category} />
+    <List fixed={fixed}>
+      {loading ? (
+        <Item key={'00'}>
+          <Category />
         </Item>
-      ))}
+      ) : (
+        categories.map((category) => (
+          <Item key={category.id}>
+            <Category {...category} />
+          </Item>
+        ))
+      )}
     </List>
   );
 
