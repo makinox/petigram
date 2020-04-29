9032e313db299916b8d8207d7deb1f4fc79c03ad
Jesus bossa <x@jesusbossa.com>
2020-04-29 08:11:20 -0500

    [ADD] List of photo cards

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import { ListOfCategories } from './components/ListOfCategories/ListOfCategories';
 import { GlobalStyles } from './globalStyles';
+import { Logo } from './components/Logo/Logo';
+import { ListOfPhotoCards } from './components/ListOfPhotoCards/ListOfPhotoCards';
+import { ListOfCategories } from './components/ListOfCategories/ListOfCategories';
 
 export const App = () => (
   <>
     <GlobalStyles />
+    <Logo />
     <ListOfCategories />
+    <ListOfPhotoCards />
   </>
 );
