11f4192e95e9ece9deac538f0b4074865b04ad11
Jesus bossa <x@jesusbossa.com>
2020-04-29 20:09:55 -0500

    [ADD] Graphql connected

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { App } from "./App";
+import React from 'react';
+import ReactDom from 'react-dom';
+import { ApolloClient, HttpLink, InMemoryCache } from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
 
-ReactDOM.render(<App />, document.getElementById("app"));
+import { App } from './App';
+
+const cache = new InMemoryCache();
+
+const link = new HttpLink({
+  uri: 'https://petgram-server-makinox.makinoxx.now.sh/graphql',
+});
+
+const client = new ApolloClient({
+  cache,
+  link,
+});
+
+ReactDom.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('app')
+);
