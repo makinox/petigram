463dea56f1c77b3982cf4343fd4c03c8a849ea60
Jesus bossa <x@jesusbossa.com>
2020-04-29 21:28:14 -0500

    [ADD] Single view

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import { GlobalStyles } from './styles/globalStyles';
 import { Logo } from './components/Logo/Logo';
-import { ListOfPhotoCards } from './components/ListOfPhotoCards/ListOfPhotoCards';
+import { ListOfPhotoCards } from './containers/ListOfPhotoCards';
 import { ListOfCategories } from './components/ListOfCategories/ListOfCategories';
+import { PhotoCardWithQuery } from './containers/PhotoCardWithQuery';
 
-export const App = () => (
-  <>
-    <GlobalStyles />
-    <Logo />
-    <ListOfCategories />
-    <ListOfPhotoCards />
-  </>
-);
+export const App = () => {
+  const urlParams = new window.URLSearchParams(window.location.search);
+  const detailId = urlParams.get('detail');
+  console.log(detailId);
+
+  return (
+    <>
+      <GlobalStyles />
+      <Logo />
+      {detailId ? (
+        <PhotoCardWithQuery id={detailId} />
+      ) : (
+        <>
+          <ListOfCategories />
+          <ListOfPhotoCards categoryId={2} />
+        </>
+      )}
+    </>
+  );
+};
--- src/components/ListOfPhotoCards/ListOfPhotoCards.js
+++ src/components/ListOfPhotoCards/ListOfPhotoCards.js
@@ -1,22 +1,7 @@
 import React from 'react';
 import { PhotoCard } from '../PhotoCard/PhotoCard';
-import { graphql } from 'react-apollo';
-import { gql } from 'apollo-boost';
 
-const withPhotos = graphql(gql`
-  query getPhotos {
-    photos {
-      id
-      categoryId
-      src
-      likes
-      userId
-      liked
-    }
-  }
-`);
-
-const ListOfPhotoCardsComponent = ({ data: { photos = [] } } = {}) => {
+export const ListOfPhotoCardsComponent = ({ data: { photos = [] } } = {}) => {
   console.log(photos);
   return (
     <ul>
@@ -26,5 +11,3 @@ const ListOfPhotoCardsComponent = ({ data: { photos = [] } } = {}) => {
     </ul>
   );
 };
-
-export const ListOfPhotoCards = withPhotos(ListOfPhotoCardsComponent);
