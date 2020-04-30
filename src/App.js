bb56dd64f6e913c6a830c687e8a6df1cfb358549
Jesus bossa <x@jesusbossa.com>
2020-04-30 16:08:51 -0500

    [ADD] Guard routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import { GlobalStyles } from './styles/globalStyles';
-import { Logo } from './components/Logo/Logo';
-import { PhotoCardWithQuery } from './containers/PhotoCardWithQuery';
 import { Home } from './pages/Home';
+import { User } from './pages/User';
+import { Favs } from './pages/Favs';
 import { Router } from '@reach/router';
 import { Detail } from './pages/Detail';
+import { Logo } from './components/Logo/Logo';
 import { NavBar } from './components/NavBar/NavBar';
+import { GlobalStyles } from './styles/globalStyles';
+import { NotRegisteredUser } from './pages/NotRegisteredUser';
+
+const UserLogged = ({ children }) => {
+  return children({ isAuth: true });
+};
 
 export const App = () => {
   return (
@@ -17,6 +23,21 @@ export const App = () => {
         <Home path="/pet/:categoryId" />
         <Detail path="/detail/:categoryId" />
       </Router>
+      <UserLogged>
+        {({ isAuth }) =>
+          isAuth ? (
+            <Router>
+              <User path="/user" />
+              <Favs path="/favs" />
+            </Router>
+          ) : (
+            <Router>
+              <NotRegisteredUser path="/user" />
+              <NotRegisteredUser path="/favs" />
+            </Router>
+          )
+        }
+      </UserLogged>
       <NavBar />
     </>
   );
