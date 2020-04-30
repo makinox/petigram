ef56b95a0439a55d1cb8ca5096dc8d1cedf6b6c6
Jesus bossa <x@jesusbossa.com>
2020-04-30 15:51:53 -0500

    [ADD] Navbar added

--- src/App.js
+++ src/App.js
@@ -4,24 +4,20 @@ import { Logo } from './components/Logo/Logo';
 import { PhotoCardWithQuery } from './containers/PhotoCardWithQuery';
 import { Home } from './pages/Home';
 import { Router } from '@reach/router';
+import { Detail } from './pages/Detail';
+import { NavBar } from './components/NavBar/NavBar';
 
 export const App = () => {
-  const urlParams = new window.URLSearchParams(window.location.search);
-  const detailId = urlParams.get('detail');
-  // console.log(detailId);
-
   return (
     <>
       <GlobalStyles />
       <Logo />
-      {detailId ? (
-        <PhotoCardWithQuery id={detailId} />
-      ) : (
-        <Router>
-          <Home path="/" />
-          <Home path="/pet/:categoryId" />
-        </Router>
-      )}
+      <Router>
+        <Home path="/" />
+        <Home path="/pet/:categoryId" />
+        <Detail path="/detail/:categoryId" />
+      </Router>
+      <NavBar />
     </>
   );
 };
--- src/components/PhotoCard/PhotoCard.js
+++ src/components/PhotoCard/PhotoCard.js
@@ -4,6 +4,7 @@ import { useNearScreen } from '../../hooks/useNearScreen';
 import { Img, ImgWrapper, Article } from './styles';
 import { FavButton } from '../FavButton/FavButton';
 import { ToggleLikeMutation } from '../../containers/ToggleLikeMutation';
+import { Link } from '@reach/router';
 const DEFAULT_IMAGE = 'https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png';
 
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
@@ -15,11 +16,11 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
     <Article ref={element}>
       {show && (
         <>
-          <a href={`/?detail=${id}`}>
+          <Link to={`/detail/${id}`}>
             <ImgWrapper>
               <Img src={src} />
             </ImgWrapper>
-          </a>
+          </Link>
           <ToggleLikeMutation>
             {(toggleLike) => {
               const handleFavClick = () => {
