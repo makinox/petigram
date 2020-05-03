d9a78900f155d55e6d36366e3f6778eb28fc9f0d
Jesus bossa <x@jesusbossa.com>
2020-05-03 12:16:25 -0500

    [ADD] Form integrated

--- src/pages/NotRegisteredUser.js
+++ src/pages/NotRegisteredUser.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import Context from '../Context';
+import { UserForm } from '../components/UserForm/UserForm';
+import { RegisterMutation } from '../containers/RegisterMutation';
 
 export const NotRegisteredUser = () => (
   <Context.Consumer>
     {({ activateAuth }) => {
       return (
-        <form onSubmit={activateAuth}>
-          <button>Iniciar sesion</button>
-        </form>
+        <>
+          <RegisterMutation>
+            {(register) => {
+              const onSubmit = ({ email, password }) => {
+                const input = { email, password };
+                const variables = { input };
+                register({ variables }).then(activateAuth);
+              };
+              return <UserForm title="Registrarse" onSubmit={onSubmit} />;
+            }}
+          </RegisterMutation>
+          <UserForm title="Iniciar sesión" onSubmit={activateAuth} />
+        </>
       );
     }}
   </Context.Consumer>
