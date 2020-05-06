355e0cf1a2060bc88d4b1e9c76e79b9068f6cda7
Jesus bossa <x@jesusbossa.com>
2020-05-06 11:28:04 -0500

    [ADD] Manifest and workbox

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackPwaManifestPlugin = require('webpack-pwa-manifest');
+const WebpackWorkboxPlugin = require('workbox-webpack-plugin');
+const path = require('path');
 
 module.exports = {
   output: {
@@ -11,6 +14,44 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: 'src/index.html',
+    }),
+    new WebpackPwaManifestPlugin({
+      name: 'Petigram - Tu app de fotos de mascotas',
+      short_name: 'Petigram 🐶',
+      description: 'Con petigram puedes encontrar fotos de animales dómesticos muy fácilmente',
+      background_color: '#fff',
+      theme_color: '#b1a',
+      orientation: 'portrait',
+      display: 'standalone',
+      start_url: '/',
+      scope: '/',
+      filename: 'manifest.webmanifest',
+      icons: [
+        {
+          src: path.resolve(__dirname, 'src/assets/icon.png'),
+          sizes: [96, 128, 192, 256, 384, 512],
+          ios: true,
+          destination: path.join('Icons'),
+        },
+      ],
+    }),
+    new WebpackWorkboxPlugin.GenerateSW({
+      runtimeCaching: [
+        {
+          urlPattern: new RegExp('https://(res.cloudinary.com|images.unsplash.com)'),
+          handler: 'CacheFirst',
+          options: {
+            cacheName: 'images',
+          },
+        },
+        {
+          urlPattern: new RegExp('https://petgram-server-makinox.makinoxx.now.sh'),
+          handler: 'NetworkFirst',
+          options: {
+            cacheName: 'api',
+          },
+        },
+      ],
     }),
   ],
   devtool: 'source-map',
